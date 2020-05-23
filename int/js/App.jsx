47f3a656269d6fac3dd9086328a1b2b9ceba7632
calssiq <leeboykt@gmail.com>
2020-05-23 18:24:14 +0700

    update file

---
+++ int/js/App.jsx
@@ -0,0 +1,19 @@
+// App.jsx
+import React from "react";
+import Hello from "./Hello";
+import {PageHeader} from "react-bootstrap";
+
+var $ = require('jquery');
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <PageHeader>
+        <div className='header-content'>
+          <Hello name="Rimini" />
+        </div>
+        </PageHeader>
+    );
+  }
+
+}
--- int/webpack.config.js
+++ int/webpack.config.js
@@ -1,10 +1,19 @@
-const webpack = require('webpack');const config = {
-    entry:  __dirname + '/js/index.jsx',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-};module.exports = config;
+const webpack = require('webpack');
+const config = {
+  entry: __dirname + '/js/index.jsx',
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+};
+module.exports = config;
