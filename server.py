3457b7a231d2c6882a1d6358d45fc5988e7b09e7
leeboykt@gmail.com <leeboykt@gmail.com>
2020-05-25 22:54:42 +0700

    first try

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_folder="../int/dist", template_folder="../int")
+import random
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+	
+def get_hello():
+    greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
+    return random.choice(greeting_list)
+
+@app.route("/hello")
+def hello():
+    return get_hello()
+
+@app.route("/plot")
+def plot():
+    return """
+    <div><img src='plot.svg'></div>
+    """
+
+if __name__ == "__main__":
+    app.run()
