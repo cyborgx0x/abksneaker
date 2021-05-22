4662783f315ba83f1b8887503753d4a11ea75f45
cyborgx0x <leeboykt@gmail.com>
2021-05-22 21:47:38 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
-class Line():
-    def __init__(self,name,age)
-    self.name = name
-    self.age = age
+class Line:
+    def __init__(self,points):
+        self.points = points
+    def drawline(self):
+        for [*index] in self.points:
+            print(index)
+   
 
-    def drawline():
-
-
-class Data():
-    
+line1 = Line([{"x":10,"y":10},{"x":5,"y":10}])
+line1.drawline()
